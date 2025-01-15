5b6c2fb261afad54db3921becd1cac14f3cd4226
Diego Brando <negradiuca@gmail.com>
2025-01-15 12:54:40 -0300

    update configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
+import deno from "@deno/astro-adapter";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: 'server'
+  output: 'server',
+  adapter: deno(),
 });
